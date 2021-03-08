01c3b2df52b18294c199d263d11aac6ce1e0808a
nico-perez <0308nperez@e-itaca.es>
2021-03-08 11:34:20 +0100

    Prácticas XPath & XQuery terminadas

--- src/main/java/dev/el_nico/dam2_ad_p4/Main.java
+++ src/main/java/dev/el_nico/dam2_ad_p4/Main.java
@@ -10,6 +10,7 @@ import org.xmldb.api.base.ResourceIterator;
 import org.xmldb.api.base.XMLDBException;
 import org.xmldb.api.modules.XMLResource;
 import org.xmldb.api.modules.XPathQueryService;
+import org.xmldb.api.modules.XQueryService;
 
 import dev.el_nico.dam2_ad_p4.comun.Pedir;
 
@@ -17,6 +18,7 @@ public class Main
 {
     private static Database db;
     private static XPathQueryService xpath;
+    private static XQueryService xquery;
 
 /* LA MOVIDA QUE ME FUNCIONÓ A MI :)
  * ResourceIterator ri = xpath.query("/productos/produc[precio=50]/stock_actual/text()").getIterator();
@@ -32,11 +34,12 @@ public class Main
         db = (Database) Class.forName("org.exist.xmldb.DatabaseImpl").newInstance();
         Collection col = Objects.requireNonNull(db.getCollection("exist://localhost:6969/exist/xmlrpc/db", "admin", ""));
         xpath = Objects.requireNonNull((XPathQueryService) col.getService(XPathQueryService.SERVICE_NAME, null));
-    
+        xquery = Objects.requireNonNull((XQueryService) col.getService(XQueryService.SERVICE_NAME, null));
+
         int orden = 0;
         do {
             imprMenu();
-            orden = Pedir.orden(10);
+            orden = Pedir.orden(19);
             acatar(orden);
         } while (orden != 0);
     }
@@ -46,6 +49,7 @@ public class Main
         System.out.println(
                   "\nÓrdenes válidas:\n================\n"
                 + "0 -> Salir.\n"
+                + "\n XPath\n -----\n"
                 + "1 -> Obtener los nodos denominación y precio de todos los productos.\n"
                 + "2 -> Obtener los nodos de los productos que sean placas base.\n"
                 + "3 -> Obtener los nodos de los productos con precio mayor de 60€ y de la zona 20.\n"
@@ -56,27 +60,268 @@ public class Main
                 + "8 -> Obtener el producto más caro.\n"
                 + "9 -> Obtener el producto más barato de la zona 20.\n"
                 + "10 -> Obtener el producto más caro de la zona 10.\n"
+                + "\n XQuery\n ------\n"
+                + "11 -> Obtener por cada zona el número de productos que tiene.\n"
+                + "12 -> Obtener la denominación de los productos entre las etiquetas <zona_>, donde _ es el número de zona.\n"
+                + "13 -> Obtener por cada zona la denominación del o de los productos más caros.\n"
+                + "14 -> Obtener la denominación de los productos contenida entra las etiquetas <placa>, <micro>, <memoria> u <otros>.\n"
+                + "15 -> Devuelve el código de sucursal y el número de cuentas que tiene de tipo AHORRO y de tipo PENSIONES.\n"
+                + "16 -> Devuelve por cada sucursal el código de sucursal, el director, la población, la suma del total saldodebe y la suma del total saldohaber de sus cuentas.\n"
+                + "17 -> Devuelve el nombre de los directores, el código de sucursal y la población de las sucursales con más de 3 cuentas.\n"
+                + "18 -> Devuelve por cada sucursal, el código de sucursal y los datos de las cuentas con más saldodebe.\n"
+                + "19 -> Devuelve la cuenta del tipo PENSIONES que ha hecho más aportación.\n"
         );
     }
 
     // llama a la funcion que corresponda
     private static void acatar(int orden) throws XMLDBException {
         switch (orden) {
-            case 0: return;
-            case 1: nodosDenominacionYPrecio(); break;
-            case 2: nodosDeProductosPlacasBase(); break;
-            case 3: nodosDeProds60EurosYZona20(); break;
-            case 4: numProdsMemoriaYZona10(); break;
-            case 5: mediaPreciosMicroprocesadores(); break;
-            case 6: datosProdsStockMinMayorQueStockActual(); break;
-            case 7: datosStockMinMayorQueStockActualYZona40(); break;
-            case 8: productoMasCaro(); break;
-            case 9: productoMasBaratoZona20(); break;
-            case 10: productoMasCaroZona10(); break;
+            case 0: default: return;
+
+            // XPath
+            case 1: xpath_1(); break;
+            case 2: xpath_2(); break;
+            case 3: xpath_3(); break;
+            case 4: xpath_4(); break;
+            case 5: xpath_5(); break;
+            case 6: xpath_6(); break;
+            case 7: xpath_7(); break;
+            case 8: xpath_8(); break;
+            case 9: xpath_9(); break;
+            case 10: xpath_10(); break;
+
+            // XQuery (productos)
+            case 11: xquery_prods_1(); break;
+            case 12: xquery_prods_2(); break;
+            case 13: xquery_prods_3(); break;
+            case 14: xquery_prods_4(); break;
+
+            // XQuery (sucursales)
+            case 15: xquery_sucur_1(); break;
+            case 16: xquery_sucur_2(); break;
+            case 17: xquery_sucur_3(); break;
+            case 18: xquery_sucur_4(); break;
+            case 19: xquery_sucur_5(); break;
         }
     }
 
-    private static void productoMasCaroZona10() throws XMLDBException {
+    /**
+     * Devuelve la cuenta del tipo PENSIONES que ha hecho más aportación.
+     * @throws XMLDBException
+     */
+    private static void xquery_sucur_5() throws XMLDBException {
+        ResourceIterator ri = xquery.query("/sucursales/sucursal/cuenta[data(@tipo) = 'PENSIONES' and aportacion = max(/sucursales/sucursal/cuenta/aportacion)]").getIterator();
+
+        while (ri.hasMoreResources()) {
+            System.out.println();
+
+            XMLResource res = (XMLResource) ri.nextResource();
+            System.out.println(res.getContent());
+        }
+    }
+
+    /**
+     * Devuelve por cada sucursal, el código de sucursal y los datos de las cuentas con más saldodebe.
+     * @throws XMLDBException
+     */
+    private static void xquery_sucur_4() throws XMLDBException {
+        ResourceIterator ri = xquery.query("for $suc in /sucursales/sucursal return (data($suc/@codigo), $suc/cuenta[saldodebe = max($suc/cuenta/saldodebe)]/*/text())").getIterator();
+
+        while (ri.hasMoreResources()) {
+            System.out.println();
+
+            XMLResource res = (XMLResource) ri.nextResource();
+            System.out.println("Cuenta con más saldodebe de la sucursal " + res.getContent());
+
+            if (ri.hasMoreResources()) {
+                res = (XMLResource) ri.nextResource();
+                System.out.println("Nombre: " + res.getContent());
+            }
+
+            if (ri.hasMoreResources()) {
+                res = (XMLResource) ri.nextResource();
+                System.out.println("Número: " + res.getContent());
+            }
+
+            if (ri.hasMoreResources()) {
+                res = (XMLResource) ri.nextResource();
+                System.out.println("Saldohaber: " + res.getContent());
+            }
+
+            if (ri.hasMoreResources()) {
+                res = (XMLResource) ri.nextResource();
+                System.out.println("Saldodebe: " + res.getContent());
+            }
+        }
+    }
+
+    /**
+     * Devuelve el nombre de los directores, el código de sucursal y
+     * la población de las sucursales con más de 3 cuentas.
+     * @throws XMLDBException
+     */
+    private static void xquery_sucur_3() throws XMLDBException {
+        ResourceIterator ri = xquery.query("for $suc in /sucursales/sucursal[count(cuenta) > 3] return (data($suc/@codigo), $suc/director/text(), $suc/poblacion/text())").getIterator();
+
+        while (ri.hasMoreResources()) {
+            System.out.println();
+
+            XMLResource res = (XMLResource) ri.nextResource();
+            System.out.println("Código: " + res.getContent());
+
+            if (ri.hasMoreResources()) {
+                res = (XMLResource) ri.nextResource();
+                System.out.println("Director: " + res.getContent());
+            }
+
+            if (ri.hasMoreResources()) {
+                res = (XMLResource) ri.nextResource();
+                System.out.println("Población: " + res.getContent());
+            }
+        }
+    }
+
+    /**
+     * Devuelve por cada sucursal el código de sucursal, el director, la población, la suma
+     * del total saldodebe y la suma del total saldohaber de sus cuentas.
+     * @throws XMLDBException
+     */
+    private static void xquery_sucur_2() throws XMLDBException {
+        ResourceIterator ri = xquery.query("for $suc in /sucursales/sucursal return (data($suc/@codigo), $suc/director/text(), $suc/poblacion/text(), sum($suc/cuenta/saldodebe), sum($suc/cuenta/saldohaber))").getIterator();
+
+        while (ri.hasMoreResources()) {
+            System.out.println();
+
+            XMLResource res = (XMLResource) ri.nextResource();
+            System.out.println("Código: " + res.getContent());
+
+            if (ri.hasMoreResources()) {
+                res = (XMLResource) ri.nextResource();
+                System.out.println("Director: " + res.getContent());
+            }
+
+            if (ri.hasMoreResources()) {
+                res = (XMLResource) ri.nextResource();
+                System.out.println("Población: " + res.getContent());
+            }
+
+            if (ri.hasMoreResources()) {
+                res = (XMLResource) ri.nextResource();
+                System.out.println("Total saldodebe: " + res.getContent());
+            }
+
+            if (ri.hasMoreResources()) {
+                res = (XMLResource) ri.nextResource();
+                System.out.println("Total saldohaber: " + res.getContent());
+            }
+        }
+
+    }
+
+    /**
+     * Devuelve el código de sucursal y el número de cuentas que tiene de tipo AHORRO
+     * y de tipo PENSIONES.
+     * @throws XMLDBException
+     */
+    private static void xquery_sucur_1() throws XMLDBException {
+        ResourceIterator ri = xquery.query("for $suc in /sucursales/sucursal return (data($suc/@codigo), count($suc/cuenta[data(@tipo)='AHORRO']), count($suc/cuenta[data(@tipo)='PENSIONES']))").getIterator();
+        while (ri.hasMoreResources()) {
+            System.out.println();
+            XMLResource nodo = ((XMLResource) ri.nextResource());
+            System.out.print("La sucursal " + nodo.getContent());
+
+            if (ri.hasMoreResources()) {
+                XMLResource n2 = (XMLResource) ri.nextResource();
+                System.out.print(" tiene " + n2.getContent() + " cuentas tipo AHORRO y ");
+            }
+
+            if (ri.hasMoreResources()) {
+                XMLResource n2 = (XMLResource) ri.nextResource();
+                System.out.println(n2.getContent() + " cuentas tipo PENSIONES");
+            }
+        }
+    }
+
+    /**
+     * Obtener la denominación de los productos contenida entra las etiquetas
+     * <placa></placa> para los productos en cuya denominación aparece la palabra
+     * Placa Base, <memoria></memoria>, para los que contienen la palabra Memoria
+     * <micro></micro>, para los que contienen la palabra Micro y <otros></otros>
+     * para el resto de productos.
+     * 
+     * @throws XMLDBException
+     */
+    private static void xquery_prods_4() throws XMLDBException {
+        ResourceIterator ri = xquery.query("(<placa>{/productos/produc/denominacion[contains(., 'Placa Base')]}</placa>,"
+                                        + "<micro>{/productos/produc/denominacion[contains(., 'Micro')]}</micro>,"
+                                        + "<memoria>{/productos/produc/denominacion[contains(., 'Memoria')]}</memoria>,"
+                                        + "<otros>{/productos/produc/denominacion[not(contains(., 'Memoria') or contains(., 'Micro') or contains(., 'Placa Base'))]}</otros>)").getIterator();
+        while (ri.hasMoreResources()) {
+            System.out.println();
+            XMLResource nodo = ((XMLResource) ri.nextResource());
+            System.out.println(nodo.getContent());
+        }
+    }
+
+    /**
+     * Obtener por cada zona la denominación del o de los productos más caros.
+     * 
+     * @throws XMLDBException
+     */
+    private static void xquery_prods_3() throws XMLDBException {
+        ResourceIterator ri = xquery.query("for $v in distinct-values(/productos/produc/cod_zona) return ($v, /productos/produc[precio = max(/productos/produc[cod_zona = $v]/precio)]/denominacion/text())").getIterator();
+        while (ri.hasMoreResources()) {
+            System.out.println();
+            XMLResource nodo = ((XMLResource) ri.nextResource());
+            System.out.print("En la zona " + nodo.getContent());
+            if (ri.hasMoreResources()) {
+                XMLResource den = ((XMLResource) ri.nextResource());
+                System.out.println(", el producto más caro es " + den.getContent());
+            }
+        }
+    }
+
+    /**
+     * Obtener la denominación de los productos entra las etiquetas <zona10></zona10>
+     * si son del código de zona 10, <zona20></zona20> si son del código de zona 20,
+     * <zona30></zona30> si son del código de zona 30 y <zona40></zona40> si son del
+     * código de zona 40.
+     * 
+     * @throws XMLDBException
+     */
+    private static void xquery_prods_2() throws XMLDBException {
+        ResourceIterator ri = xquery.query("for $v in distinct-values(/productos/produc/cod_zona) return element{ 'zona' || $v }{ /productos/produc[cod_zona = $v]/denominacion }").getIterator();
+        while (ri.hasMoreResources()) {
+            System.out.println();
+            XMLResource nodo = ((XMLResource) ri.nextResource());
+            System.out.println(nodo.getContent());
+        }
+    }
+
+    /**
+     * Obtener por cada zona el número de productos que tiene.
+     * @throws XMLDBException
+     */
+    private static void xquery_prods_1() throws XMLDBException {
+        ResourceIterator ri = xquery.query("for $v in distinct-values(/productos/produc/cod_zona) return ($v, count(/productos/produc[cod_zona = $v]))").getIterator();
+
+        while (ri.hasMoreResources()) {
+            System.out.println();
+            XMLResource zona = ((XMLResource) ri.nextResource());
+            System.out.print("La zona " + zona.getContent());
+            if (ri.hasMoreResources()) {
+                XMLResource count = ((XMLResource) ri.nextResource());
+                System.out.println(" tiene " + count.getContent() + " productos");
+            }
+        }
+    }
+
+    /**
+     * Obtener el producto más caro de la zona 10.
+     * 
+     * @throws XMLDBException
+     */
+    private static void xpath_10() throws XMLDBException {
         ResourceIterator ri = xpath.query("/productos/produc[precio = max(/productos/produc[cod_zona = 10]/precio)]").getIterator();
         while (ri.hasMoreResources()) {
             System.out.println();
@@ -85,7 +330,11 @@ public class Main
         }
     }
 
-    private static void productoMasBaratoZona20() throws XMLDBException {
+    /**
+     * Obtener el producto más barato de la zona 20.
+     * @throws XMLDBException
+     */
+    private static void xpath_9() throws XMLDBException {
         ResourceIterator ri = xpath.query("/productos/produc[precio = min(/productos/produc[cod_zona = 20]/precio)]").getIterator();
         while (ri.hasMoreResources()) {
             System.out.println();
@@ -94,7 +343,11 @@ public class Main
         }
     }
 
-    private static void productoMasCaro() throws XMLDBException {
+    /**
+     * Obtener el producto más caro.
+     * @throws XMLDBException
+     */
+    private static void xpath_8() throws XMLDBException {
 
         ResourceIterator ri = xpath.query("/productos/produc[precio = max(/productos/produc/precio)]").getIterator();
         while (ri.hasMoreResources()) {
@@ -104,7 +357,12 @@ public class Main
         }
     }
 
-    private static void datosStockMinMayorQueStockActualYZona40() throws XMLDBException {
+    /**
+     * Obtener el nombre del producto y el precio de aquellos cuyo stock mínimo sea mayor 
+     * que su stock actual y sean de la zona 40.
+     * @throws XMLDBException
+     */
+    private static void xpath_7() throws XMLDBException {
 
         ResourceIterator ri = xpath.query("/productos/produc/*[(self::denominacion or self::precio) and number(../stock_minimo/text()) > number(../stock_actual/text()) and ../cod_zona/text() = 40]/text()").getIterator();
 
@@ -120,7 +378,11 @@ public class Main
         }
     }
 
-    private static void datosProdsStockMinMayorQueStockActual() throws XMLDBException {
+    /**
+     * Obtener los datos de los productos cuyo stock mínimo sea mayor que su stock actual.
+     * @throws XMLDBException
+     */
+    private static void xpath_6() throws XMLDBException {
         ResourceIterator ri = xpath.query("/productos/produc[number(stock_minimo) > number(stock_actual)]").getIterator();
         
         while (ri.hasMoreResources()) {
@@ -138,7 +400,11 @@ public class Main
         }
     }
 
-    private static void mediaPreciosMicroprocesadores() throws XMLDBException {
+    /**
+     * Obtener la media de precio de los micros.
+     * @throws XMLDBException
+     */
+    private static void xpath_5() throws XMLDBException {
         ResourceIterator ri = xpath.query("sum(/productos/produc[denominacion[contains(., 'Micro')]]/precio/text()) div count(/productos/produc[denominacion[contains(., 'Micro')]])").getIterator();
     
         while (ri.hasMoreResources()) {
@@ -148,7 +414,11 @@ public class Main
         }
     }
 
-    private static void numProdsMemoriaYZona10() throws XMLDBException {
+    /**
+     * Obtener el número de productos que sean memorias y de la zona 10.
+     * @throws XMLDBException
+     */
+    private static void xpath_4() throws XMLDBException {
         ResourceIterator ri = xpath.query("count(/productos/produc[denominacion[contains(., 'Memoria')] and cod_zona[text() = 10]])").getIterator();
 
         while (ri.hasMoreResources()) {
@@ -158,7 +428,11 @@ public class Main
         }
     }
 
-    private static void nodosDeProds60EurosYZona20() throws XMLDBException {
+    /**
+     * Obtener los nodos de los productos con precio mayor de 60€ y de la zona 20.
+     * @throws XMLDBException
+     */
+    private static void xpath_3() throws XMLDBException {
         ResourceIterator ri = xpath.query("/productos/produc[precio[text() > 60] and cod_zona[text() = 20]]").getIterator();
 
         while (ri.hasMoreResources()) {
@@ -168,7 +442,11 @@ public class Main
         }
     }
 
-    private static void nodosDeProductosPlacasBase() throws XMLDBException {
+    /**
+     * Obtener los nodos de los productos que sean placas base.
+     * @throws XMLDBException
+     */
+    private static void xpath_2() throws XMLDBException {
         ResourceIterator ri = xpath.query("/productos/produc[denominacion[contains(., 'Placa Base')]]").getIterator();
 
         while (ri.hasMoreResources()) {
@@ -178,7 +456,11 @@ public class Main
         }
     }
 
-    private static void nodosDenominacionYPrecio() throws XMLDBException {
+    /**
+     * Obtener los nodos denominación y precio de todos los productos.
+     * @throws XMLDBException
+     */
+    private static void xpath_1() throws XMLDBException {
         ResourceIterator ri = xpath.query("/productos/produc/*[self::denominacion or self::precio]").getIterator();
 
         while (ri.hasMoreResources()) {
